0f730dfd186795029cc8079ed1a547329606dd3a
Waihlyanhtet <waihlyanhtet0456@gmail.com>
2026-06-21 14:02:25 +0630

    J

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import numpy as np
 from datetime import datetime, timedelta, timezone
 
 BOT_TOKEN = '8986263556:AAEcSDe4CudG9jF64t8mUPSLT5smN7s-yqs'
-GITHUB_TOKEN= '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'
+GITHUB_TOKEN= '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'
 ADMIN_ID = "6417845887"
 REPO_OWNER = "Waihlyanhtet"
 REPO_NAME = "abc"

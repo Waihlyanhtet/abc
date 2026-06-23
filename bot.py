49380db47d30ee8d6e837368988789cd8d1721b9
Waihlyanhtet <waihlyanhtet0456@gmail.com>
2026-06-23 19:14:00 +0630

    Change GITHUB_TOKEN in bot.py
    Updated GITHUB_TOKEN to a new value.

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import numpy as np
 from datetime import datetime, timedelta, timezone
 
 BOT_TOKEN = '8986263556:AAEcSDe4CudG9jF64t8mUPSLT5smN7s-yqs'
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", "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")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", "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")
 ADMIN_ID = "6417845887"
 REPO_OWNER = "Waihlyanhtet"
 REPO_NAME = "abc"
